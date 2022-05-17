3cff31be9cb3926af8e170427b45d6b8d640bc63
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-05-18 02:59:35 +0600

    scrolling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-
-import './App.css'
-import Featured from './featured/Featured'
-import Footer from './header_footer/Footer'
-import Header from './header_footer/Header'
-import Highlights from './highlights/Highlihts'
-import Location from './location/Location'
-import Pricing from './pricing/Pricing'
-import './resources/styles.css'
-import VenueInfo from './venue_info/VenueInfo'
+import Element from "react-scroll/modules/components/Element";
+import "./App.css";
+import Featured from "./featured/Featured";
+import Footer from "./header_footer/Footer";
+import Header from "./header_footer/Header";
+import Highlights from "./highlights/Highlihts";
+import Location from "./location/Location";
+import Pricing from "./pricing/Pricing";
+import "./resources/styles.css";
+import VenueInfo from "./venue_info/VenueInfo";
 function App() {
   return (
     <div className="App">
-      <Header ></Header>
-      <Featured />
-      <VenueInfo />
-      <Highlights />
-      <Pricing />
-      <Location />
-      <Footer />
+      <Element name="eventStarts">
+        <Header />
+      </Element>
+      <Element name="venueinfo">
+        <Featured />
+      </Element>
+      <Element name="highlights">
+        <VenueInfo />
+      </Element>
+      <Element name="pricing">
+        <Highlights />
+      </Element>
+      <Element name="">
+        <Pricing />
+      </Element>
+      <Element name="">
+        <Location />
+      </Element>
+      <Element name="">
+        <Footer />
+      </Element>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/header_footer/SideDrawer.jsx
+++ src/header_footer/SideDrawer.jsx
@@ -1,7 +1,17 @@
 import { Drawer, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 import React from 'react';
+import scroller from 'react-scroll/modules/mixins/scroller';
 
 const SideDrawer = ({open, onClose}) => {
+    const scrollToElement = (element) => {
+        scroller.scrollTo(element, {
+            duration: 1500,
+            delay: 100,
+            smooth: true,
+            offset: -150,
+        });
+        onClose(onclose)
+    }
     return (
        
             <Drawer open={open}
@@ -10,22 +20,22 @@ const SideDrawer = ({open, onClose}) => {
             >
                 <List component='nav'>
                     <ListItem disablePadding>
-                        <ListItemButton component="a" href="#simple-list">
+                        <ListItemButton component="a"  onClick={() => scrollToElement('eventStarts')}>
                             <ListItemText primary = "Event Starts In" />
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
-                        <ListItemButton component="a" href="#simple-list">
+                        <ListItemButton component="a"  onClick={() => scrollToElement('venueinfo')}>
                             <ListItemText primary = "Venue Info" />
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
-                        <ListItemButton component="a" href="#simple-list">
+                        <ListItemButton component="a"  onClick={() => scrollToElement('highlights')}>
                             <ListItemText primary = "Highlights" />
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
-                        <ListItemButton component="a" href="#simple-list">
+                        <ListItemButton component="a"  onClick={() => scrollToElement('pricing')}>
                             <ListItemText primary = "Pricing" />
                         </ListItemButton>
                     </ListItem>
